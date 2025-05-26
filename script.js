21c513f5dc74b7239f9ceafb918184c855691d29
mizaelzin10 <164011846+mizaelzin10@users.noreply.github.com>
2025-05-26 14:31:14 -0300

    kkksskk

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+[
+{
+anunciado: "Pergunta 1",
+alternativado: ["Alternativa 1", "Alternativa 2"],
+},
+{
+anunciado: "Pergunta 2",
+alternativado: ["Alternativa 1", "Alternativa 2"],
+},
+];
